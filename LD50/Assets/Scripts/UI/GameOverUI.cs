b33ec5b14437db87cc57edb9b4d1007913806346
Mozetor <maurice-zerressen@web.de>
2022-04-02 16:01:55 +0200

    Fix game over scene

--- LD50/Assets/Scripts/UI/GameOverUI.cs
+++ LD50/Assets/Scripts/UI/GameOverUI.cs
@@ -11,11 +11,14 @@ namespace LD50.UI
         public TextMeshProUGUI r1;
         public TextMeshProUGUI r2;
         public TextMeshProUGUI r3;
+        public TextMeshProUGUI g0;
         public TextMeshProUGUI g1;
         public TextMeshProUGUI g2;
         public TextMeshProUGUI g3;
         public TextMeshProUGUI g4;
         
+        private const string S0 = "Highest Score: {0}";
+        private const string S1B = "Score Sum: {0}";
         private const string S1 = "Score: {0}";
         private const string S2 = "Icebergs dodged: {0}";
         private const string S3 = "PowerUp's collected: {0}";
@@ -23,7 +26,8 @@ namespace LD50.UI
 
         private void Start()
         {
-            g1.text = string.Format(S1, globalStats.timeSurvived);
+            g0.text = string.Format(S0, globalStats.timeSurvivedHighScore);
+            g1.text = string.Format(S1B, globalStats.timeSurvived);
             g2.text = string.Format(S2, globalStats.icebergsDodged);
             g3.text = string.Format(S3, globalStats.powerUpsCollected);
             g4.text = string.Format(S4, globalStats.roundsPlayed);
