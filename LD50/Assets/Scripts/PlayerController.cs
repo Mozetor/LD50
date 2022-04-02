040d502be6a4f8516306a5913ead7f59023baea8
Florian Scharping <Floscha62@web.de>
2022-04-02 14:53:28 +0200

    Ship turns

--- LD50/Assets/Scripts/PlayerController.cs
+++ LD50/Assets/Scripts/PlayerController.cs
@@ -1,29 +1,32 @@
 using UnityEngine;
 
-namespace LD50
-{
-    public class PlayerController : MonoBehaviour
-    {
+namespace LD50 {
+    public class PlayerController : MonoBehaviour {
         public float maneuverability;
+        public float laneSwapThreshold;
         private int _lane;
         private GameManager _gameManager;
         private Rigidbody _rigidbody;
 
-        private void Awake()
-        {
+        private void Awake() {
             _gameManager = FindObjectOfType<GameManager>();
             _rigidbody = FindObjectOfType<Rigidbody>();
             _lane = _gameManager.laneCount / 2;
         }
 
-        private void Update()
-        {
+        private void Update() {
             var laneDiff = 0;
 
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            Debug.LogFormat("Position {0}, Target position {1}", transform.position.x, _gameManager.GetLaneXPosition(_lane));
+
+            if (Mathf.Abs(transform.position.x - _gameManager.GetLaneXPosition(_lane)) < laneSwapThreshold &&
+                (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            ) {
                 laneDiff--;
             }
-            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            if (Mathf.Abs(transform.position.x - _gameManager.GetLaneXPosition(_lane)) < laneSwapThreshold &&
+                (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            ) {
                 laneDiff++;
             }
 
@@ -31,11 +34,31 @@ namespace LD50
         }
 
         private void FixedUpdate() {
-            
-            _rigidbody.MovePosition(Vector3.Lerp(
-                this.transform.position, 
-                Vector3.right * _gameManager.GetLaneXPosition(_lane), 
-                Time.fixedDeltaTime * maneuverability)
+            var distance = (_gameManager.GetLaneXPosition(_lane) - transform.position.x) /
+                (_gameManager.xDistanceBetweenLane + laneSwapThreshold);
+
+            var distanceToEase = 1 - Mathf.Abs(distance);
+
+            // distance = -1 -> 0.5f
+            // distance = -0.5 -> 0
+            // distance = 0 -> 0.5f
+            // distance = 0.5 -> 1
+            // distance = -1 -> 0.5f
+            var distanceToAngleTime = 0.5f * Mathf.Sin(distance * Mathf.PI) + 0.5f;
+
+
+            _rigidbody.MoveRotation(Quaternion.Lerp(
+                Quaternion.Euler(0, -20, 0),
+                Quaternion.Euler(0, 20, 0),
+                distanceToAngleTime
+            ));
+
+            _rigidbody.MovePosition(
+                Vector3.Lerp(
+                    this.transform.position,
+                    Vector3.right * _gameManager.GetLaneXPosition(_lane),
+                    Time.fixedDeltaTime * maneuverability * distanceToEase
+                )
            );
         }
     }
