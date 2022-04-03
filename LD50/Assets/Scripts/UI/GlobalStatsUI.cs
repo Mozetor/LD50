53320414b617742358ab392b5a268d2a72fab689
Mozetor <maurice-zerressen@web.de>
2022-04-03 17:13:26 +0200

    add global stats to menu

--- LD50/Assets/Scripts/UI/GlobalStatsUI.cs
+++ LD50/Assets/Scripts/UI/GlobalStatsUI.cs
@@ -3,14 +3,10 @@ using UnityEngine;
 
 namespace LD50.UI
 {
-    public class GameOverUI : MonoBehaviour
+    public class GlobalStatsUI : MonoBehaviour
     {
         public GameStats globalStats;
-        public GameStats roundStats;
-
-        public TextMeshProUGUI r1;
-        public TextMeshProUGUI r2;
-        public TextMeshProUGUI r3;
+        
         public TextMeshProUGUI g0;
         public TextMeshProUGUI g1;
         public TextMeshProUGUI g2;
@@ -19,7 +15,6 @@ namespace LD50.UI
         
         private const string S0 = "Highest Score: {0}";
         private const string S1B = "Score Sum: {0}";
-        private const string S1 = "Score: {0}";
         private const string S2 = "Icebergs dodged: {0}";
         private const string S3 = "PowerUp's collected: {0}";
         private const string S4 = "Rounds played: {0}";
@@ -31,9 +26,6 @@ namespace LD50.UI
             g2.text = string.Format(S2, globalStats.icebergsDodged);
             g3.text = string.Format(S3, globalStats.powerUpsCollected);
             g4.text = string.Format(S4, globalStats.roundsPlayed);
-            r1.text = string.Format(S1, roundStats.timeSurvived.ToString("0.00"));
-            r2.text = string.Format(S2, roundStats.icebergsDodged);
-            r3.text = string.Format(S3, roundStats.powerUpsCollected);
         }
     }
 }
